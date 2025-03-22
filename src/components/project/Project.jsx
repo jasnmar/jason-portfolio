bf4d61159b99db1641e49281f11c4a43aa3953a8
jasnmar <jasnmar@gmail.com>
2025-03-22 12:59:05 -0400

    Updates

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -7,27 +7,27 @@ function Project({ data }) {
   let goalList = "";
   if (projectGoals?.length > 0) {
     goalList = projectGoals.map((goal) => {
-      return <li key={goal}>{goal}</li>
+      return <li className="project--item-card" key={goal}>{goal}</li>
     })
   }
 
   return (
     <>
       <div className="project-card">
-        <h2 className="project-title">{data.name}</h2>
-        <div className="project-main">
-          <img className="project-main-image" src={data.image}></img>
-          <div className="project-details">
-            <p className="project-description">{data.description}</p>
-            <a className="project-link" target="_blank" href={data.source}>
+        <h2 className="project--title">{data.name}</h2>
+        <div className="project--main">
+          <img className="project--main-image" src={data.image}></img>
+          <div className="project--details">
+            <p className="project--description">{data.description}</p>
+            <a className="project--link" target="_blank" href={data.source}>
               Source Code
             </a>
             <br></br>
-            <a className="project-link" target="_blank" href={data.deploy}>
+            <a className="project--link" target="_blank" href={data.deploy}>
               Deployment
             </a>
-            {goalList ? <p className="project-list-header">Learning Goals:</p> : ""}
-            {goalList ? <ul>{goalList}</ul>: ""}
+            {goalList ? <p className="project--list-header">Learning Goals:</p> : ""}
+            {goalList ? <ul className="project--list">{goalList}</ul>: ""}
 
           </div>
         </div>
