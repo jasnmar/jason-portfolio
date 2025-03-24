3607e8b9aa2fa736858c26b73da73c655e480422
jasnmar <jasnmar@gmail.com>
2025-03-24 13:34:09 -0400

    Image Updates

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,5 +1,7 @@
 import "./Project.css";
 import PropTypes from "prop-types"
+import { v4 as uuidv4 } from 'uuid'
+
 
 
 function Project({ data }) {
@@ -11,27 +13,42 @@ function Project({ data }) {
     })
   }
 
+  const imageArray = data.images
+  let imageList = []
+  if(imageArray?.length > 0) {
+    imageList = imageArray.map((imageObj) => {
+      return <img key={uuidv4()} className="project--image" alt={data.name + " screenshots"} src={imageObj.image}></img>
+    })
+  } 
+  
   return (
     <>
-      <div className="project-card">
+      <section className="project--card">
         <h2 className="project--title">{data.name}</h2>
         <div className="project--main">
-          <img className="project--main-image" src={data.image}></img>
+          {imageList.length > 0 ? 
+            <div className="project--image-container">
+              {imageList}
+            </div> : 
+            <img className="project--main-image" alt={data.name + "screenshots"} src={data.image}></img>}
+            
+          
           <div className="project--details">
             <p className="project--description">{data.description}</p>
-            <a className="project--link" target="_blank" href={data.source}>
-              Source Code
-            </a>
-            <br></br>
-            <a className="project--link" target="_blank" href={data.deploy}>
-              Deployment
-            </a>
-            {goalList ? <p className="project--list-header">Learning Goals:</p> : ""}
+            <div className="project--links">
+              <a className="project--link" target="_blank" href={data.source}>
+                Source Code
+              </a>
+              <br></br>
+              {data.deploy ? <a className="project--link" target="_blank" href={data.deploy}>
+                Deployment
+              </a> : <p>Not Deployed</p>}
+            </div>
+            {goalList ? <p className="project--list-header">Learning Goals</p> : ""}
             {goalList ? <ul className="project--list">{goalList}</ul>: ""}
-
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
