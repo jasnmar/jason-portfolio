0958b8d3f5923a14a784bd473127706153b6f162
jasnmar <jasnmar@gmail.com>
2025-05-03 15:47:45 -0400

    Some fixes to image backgrounds.

--- src/components/ImageItem/ImageItem.jsx
+++ src/components/ImageItem/ImageItem.jsx
@@ -1,14 +1,48 @@
 import "./ImageItem.css"
+import { useEffect, useState } from "react"
 import PropTypes from "prop-types"
 
+
 function ImageItem({ src , alt, itemId, clickAction}) {
+  const [imageDimensions, setImageDimensions] = useState({width: 0, height: 0})
 
   //Create an image element so we can get some data from it
-  const img = document.createElement("img")
-  img.src = src
-  
-  const imageWidth = img.width
-  const imageHeight = img.height
+
+  const imageWidth = imageDimensions.width
+  const imageHeight = imageDimensions.height
+  console.log('imageDimensions: ', imageDimensions)
+
+  useEffect(() => {
+    // Flag to prevent state update on unmounted component
+    let isMounted = true
+    async function getImageProps(imageSource) {
+      const img = document.createElement("img")
+      img.src = imageSource
+      try {
+        await img.decode()
+        console.log(
+          "Image decoded: ",
+          imageSource,
+          "Dimensions:",
+          img.naturalWidth,
+          img.naturalHeight
+        )
+        if (isMounted) {
+          setImageDimensions({
+            width: img.naturalWidth,
+            height: img.naturalHeight
+          })
+        }
+      } catch (error) {
+        console.error("Error decoding image:", imageSource, error)
+        setImageDimensions({ width: 0, height: 0 })
+      }
+    }
+    getImageProps(src)
+    return () => {
+      isMounted = false
+    }
+  }, [src])
 
   //I'm currently targeting a 5/4 image aspect ration. That may need to change
   let heightConstrained
